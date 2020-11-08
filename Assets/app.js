f82d1567ab77054e6e7b3893bf60e304122dddf4
Franco <phanfranco@yahoo.com>
2020-11-07 19:31:16 -0800

    This is too hard

--- Assets/app.js
+++ Assets/app.js
@@ -1,25 +1,30 @@
-var inputCityEl = docutment.getElementById("city-form");
+var inputCityEl = document.getElementById("city-form");
 var searchEl = document.getElementById("city-button");
+var cityNameEl = document.getElementById("city-name");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindEel = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
 
+const APIkey = "af827e466de01437ddc96d9009bdc6d0";
 
-
-
-
-function getWeather() {
-    var key ='af827e466de01437ddc96d9009bdc6d0';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + '5378538' + '&appid=' + key)  
+function getWeather(cityName) {
+    let URL = 'https://api.openweathermap.org/data/2.5/weather?id=' + cityName + '&appid=' + APIkey;
+    fetch(URL)  
     .then(function(resp) { 
+        console.log(resp) })
         // Convert data to json
-        console.log(resp)
-        return resp.json() }) 
-    .then(function(data) {
-        console.log(data);
-        console.log(data.name) })
-        // catch any errors
-    .catch(function(err) {
-        console.log(err);
-    });
-  }
-//   console.log(getWeather)
+//         console.log(resp)
+        return resp.json() } 
+    // .then(function(data) {
+//         console.log(data);
+//         console.log(data.name) })
+//         // catch any errors
+//     .catch(function(err) {
+//         console.log(err);
+//     });
+
+// //   console.log(getWeather)
   getWeather()
-//   console.log(getWeather())
+// //   console.log(getWeather())
+    
