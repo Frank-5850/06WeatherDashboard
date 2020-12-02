1d7809de71442762016b29e96d8cfe5da68da61d
Franco <phanfranco@yahoo.com>
2020-12-01 22:41:03 -0800

    Made the forecast repopulate properly

--- Assets/app.js
+++ Assets/app.js
@@ -16,12 +16,36 @@ var threeDay= document.getElementById("threeDaysAfter");
 var fourDay= document.getElementById("fourDaysAfter");
 var fiveDay= document.getElementById("fiveDaysAfter");
 var currentIcon = document.getElementById("dailyIcon");
+var titleHistory = document.getElementById("titleHistory")
+var searchHistory = document.getElementById("searchHistory")
 
 // API Key
 const APIkey = "d91f911bcf2c0f925fb6535547a5ddc9";
 
 // weather data
 var weather = {};
+
+function combineListeners () {
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    var text = inputCityEl.value;
+      function storeCity () {
+      window.localStorage.setItem("history", JSON.stringify(text));
+    }
+    storeCity()
+    return getWeather(text);
+  });
+  searchEl.addEventListener("click", function (e) {
+    e.preventDefault();
+    var text = inputCityEl.value;
+    function storeCity () {
+      window.localStorage.setItem("history", JSON.stringify(text));
+    }
+    storeCity()
+    return getWeather(text)
+  });
+}
+combineListeners();
 
 // function that gathers data from API and adds to weather object
 function getWeather(cityName) {
@@ -35,47 +59,18 @@ function getWeather(cityName) {
       console.log(data);
       weather.name = data.city.name;
       weather.data = data.list;
-      console.log(weather)
+      // console.log(weather)
       })
     .then(function(){
       showWeather();
     });
   }
 
-function combineListeners () {
-  form.addEventListener("submit", function(e){
-    e.preventDefault();
-    var text = inputCityEl.value;
-      function storeCity () {
-      window.localStorage.setItem("history", JSON.stringify(text));
-      console.log("hello")
-    }
-    storeCity()
-    return getWeather(text);
-  });
-  searchEl.addEventListener("click", function (e) {
-    e.preventDefault();
-    var text = inputCityEl.value;
-    function storeCity () {
-      window.localStorage.setItem("history", JSON.stringify(text));
-      console.log("hello")
-    }
-    storeCity()
-    return getWeather(text)
-  });
-}
-combineListeners();
-
-// This gets the current date
-var today = new Date();
-console.log(today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear())
-
-
+var titleHist = false
 // Display to the HTML
 function showWeather() {
 
   for (let i = 0; i < weather.data.length; i++) {
-    // console.log(weather.data[i])
     var date = new Date(weather.data[i].dt * 1000)
     var days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat']
     var newDay = days[date.getDay()]
@@ -83,7 +78,6 @@ function showWeather() {
     weather.data[i].dt = newDay
     weather.data[i].dt_txt = newDate
   }
-
 
   currentDay.innerHTML = `${weather.data[0].dt_txt}`;
   cityNameEl.innerHTML = `${weather.name}`;
@@ -99,6 +93,7 @@ function showWeather() {
     <img src="./Assets/Icons/${weather.data[6].weather[0].icon}.png" /><div>${weather.data[6].weather[0].description}</div>
     <div><span>${Math.floor((weather.data[6].main.feels_like - 273)* 9/5 + 32)}° F</span></div>
     <div>Humidity: ${weather.data[6].main.humidity}%</div>`;
+  oneDay.innerHTML = "";
   oneDay.appendChild(dayOne);
 
   var dayTwo = document.createElement("div");
@@ -106,6 +101,7 @@ function showWeather() {
     <img src="./Assets/Icons/${weather.data[14].weather[0].icon}.png" /><div>${weather.data[14].weather[0].description}</div>
     <div><span>${Math.floor((weather.data[14].main.feels_like - 273)* 9/5 + 32)}° F</span></div>
     <div>Humidity: ${weather.data[14].main.humidity}%</div>`;
+  twoDay.innerHTML = "";
   twoDay.appendChild(dayTwo);
 
   var dayThree = document.createElement("div");
@@ -113,6 +109,7 @@ function showWeather() {
     <img src="./Assets/Icons/${weather.data[22].weather[0].icon}.png" /><div>${weather.data[22].weather[0].description}</div>
     <div><span>${Math.floor((weather.data[22].main.feels_like - 273)* 9/5 + 32)}° F</span></div>
     <div>Humidity: ${weather.data[22].main.humidity}%</div>`;
+  threeDay.innerHTML = "";
   threeDay.appendChild(dayThree);
 
   var dayFour = document.createElement("div");
@@ -120,6 +117,7 @@ function showWeather() {
     <img src="./Assets/Icons/${weather.data[30].weather[0].icon}.png" /><div>${weather.data[30].weather[0].description}</div>
     <div><span>${Math.floor((weather.data[30].main.feels_like - 273)* 9/5 + 32)}° F</span></div>
     <div>Humidity: ${weather.data[30].main.humidity}%</div>`;
+  fourDay.innerHTML = "";
   fourDay.appendChild(dayFour);
 
   var dayFive = document.createElement("div");
@@ -127,5 +125,21 @@ function showWeather() {
     <img src="./Assets/Icons/${weather.data[38].weather[0].icon}.png" /><div>${weather.data[38].weather[0].description}</div>
     <div><span>${Math.floor((weather.data[38].main.feels_like - 273)* 9/5 + 32)}° F</span></div>
     <div>Humidity: ${weather.data[38].main.humidity}%</div>`;
+  fiveDay.innerHTML = "";
   fiveDay.appendChild(dayFive);
+
+
+  if (titleHist === false) {
+    var title = document.createElement("div")
+    title.innerHTML= `<p>This is your search history:</p>`
+    titleHistory.append(title)
+    titleHist = true
+  };
+
+  var history = document.createElement("div")
+  history.innerHTML = `<div>${window.localStorage.getItem("history")}</div>`
+  searchHistory.prepend(history)
+
+  console.log(weather)
 };
+
